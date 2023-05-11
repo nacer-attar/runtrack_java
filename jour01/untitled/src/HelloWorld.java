99f3d7122b94827e046fc94147d019de9df0574f
Nacer Eddine Attar <nacer.attar@laplateforme.io>
2023-05-11 15:14:39 +0200

    code commenté

---
+++ jour01/untitled/src/HelloWorld.java
@@ -0,0 +1,10 @@
+// Déclaration de la classe HelloWorld
+public class HelloWorld {
+
+    // Déclaration de la méthode main
+    public static void main(String[] args) {
+
+        // Affichage de "Hello World !" dans la console
+        System.out.println("Hello World !");
+    }
+}
