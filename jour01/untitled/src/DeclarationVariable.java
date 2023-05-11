6f5b6bf9251378681ceb09ec9175db9b9baeaf13
Nacer Eddine Attar <nacer.attar@laplateforme.io>
2023-05-11 16:25:46 +0200

    job04 done

---
+++ jour01/untitled/src/DeclarationVariable.java
@@ -0,0 +1,6 @@
+public class DeclarationVariable {
+    public static void main(String[] args) {
+        int a, b, c; // déclaration des variables sur une ligne
+        a = b = c = 0; // initialisation des variables à 0 sur une deuxième ligne
+    }
+}
