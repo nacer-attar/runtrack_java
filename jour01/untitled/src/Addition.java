9099694a843854c0989f84941e9518df59adfddc
Nacer Eddine Attar <nacer.attar@laplateforme.io>
2023-05-11 19:18:52 +0200

    job07 done

---
+++ jour01/untitled/src/Addition.java
@@ -0,0 +1,12 @@
+public class Addition {
+    public static void main (String[] args) {
+        int a = 3;
+        int b = 73;
+        /* double pour afficher des décimales
+        (double) pour forcer la conversion de a et b avant la division
+         pour que le résultat ne soit pas un entier
+         */
+        double res = (double) a/b;
+        System.out.println(res);
+    }
+}
